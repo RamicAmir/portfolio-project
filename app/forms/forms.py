231c8a91043ed5fb2cc4b3acefd110c80cbcc919
Amer Ahmed <amer.ahmed@studerande.plushogskolan.se>
2022-02-02 11:56:53 +0100

    Added user-account-profiel and updated content

--- app/forms/forms.py
+++ app/forms/forms.py
@@ -1,5 +1,7 @@
+from flask_login import current_user
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from flask_wtf.file import FileAllowed
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, FileField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from app.models.models import User
 
@@ -27,3 +29,22 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Sign in')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=60)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpeg', 'jpg', 'png'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one!')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose a different one!')
--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,5 +1,8 @@
-from flask import render_template, flash, redirect, url_for, request
-from app.forms.forms import RegistrationForm, LoginForm
+import os
+import secrets
+from PIL import Image
+from flask import render_template, flash, redirect, url_for, request, current_app
+from app.forms.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from app import app, db, bcrypt
 from app.models.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
@@ -68,11 +71,37 @@ def logout():
     return redirect(url_for('index'))
 
 
-@app.route('/account')
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(current_app.root_path, 'static/profile_pics', picture_fn)
+
+    output_size = (125, 125)
+    img = Image.open(form_picture)
+    img.thumbnail(output_size)
+    img.save(picture_path)
+    return picture_fn
+
+
+@app.route('/account', methods=['GET', 'POST'])
 @login_required
 def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.profile = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your Account has been Updated', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
     profiles = url_for('static', filename='profile_pics/' + current_user.profile)
-    return render_template('account.html', profile=profiles)
+    return render_template('account.html', profile=profiles, form=form)
 
 
 @app.errorhandler(404)
