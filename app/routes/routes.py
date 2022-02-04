dad83e0b88fc90986e5bba0e3e8437e862bfac17
Amer Ahmed <amer.ahmed@studerande.plushogskolan.se>
2022-02-04 16:47:35 +0100

    Added user_post

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -12,7 +12,8 @@ from app.models.models import User, Post
 
 @app.route("/")
 def index():
-    posts = Post.query.all()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.published.desc()).paginate(page=page, per_page=5)
     return render_template('index.html', posts=posts)
 
 
@@ -113,32 +114,42 @@ def post(post_id):
 @app.route('/post/<int:post_id>/update', methods=['GET', 'POST'])
 @login_required
 def update_post(post_id):
-        post = Post.query.get_or_404(post_id)
-        if post.author != current_user:
-            abort(403)
-        form = PostForm()
-        if form.validate_on_submit():
-            post.title = form.title.data
-            post.content = form.content.data
-            db.session.commit()
-            flash('Your post has been updated!', 'success')
-            return redirect(url_for('post', post_id=post.id))
-        elif request.method == 'GET':
-            form.title.data = post.title
-            form.content.data = post.content
-        return render_template('posts/create_posts.html', form=form, legend='Update Post')
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('posts/create_posts.html', form=form, legend='Update Post')
 
 
-@app.route('/post/<int:post_id>/delete', methods=['POST'])
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
 @login_required
 def delete_post(post_id):
-        post = Post.query.get_or_404(post_id)
-        if post.author != current_user:
-            abort(403)
-            db.session.delete(post)
-            db.session.commit()
-            flash('Your post has been deleted!', 'success')
-        return redirect(url_for('index'))
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('index'))
+
+
+@app.route('/user/<string:username>')
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username). first_or_404()
+    posts = Post.query.filter_by(author=user)\
+        .order_by(Post.published.desc())\
+        .paginate(page=page, per_page=5)
+    return render_template('posts/user_posts.html', posts=posts, user=user)
 
 
 @app.errorhandler(404)
