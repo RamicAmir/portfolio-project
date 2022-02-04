7194db7c13477baef31f4bd0e9fb156ab7f17e9b
Amer Ahmed <amer.ahmed@studerande.plushogskolan.se>
2022-02-04 11:51:50 +0100

    Added update_post/delet_post and buttom for both update als delet

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -2,7 +2,7 @@ import os
 import secrets
 from PIL import Image
 from app import app, db, bcrypt
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, abort
 from flask import url_for, request, current_app
 from app.forms.forms import RegistrationForm, LoginForm
 from app.forms.forms import UpdateAccountForm, PostForm
@@ -108,6 +108,37 @@ def create_posts():
 def post(post_id):
     post = Post.query.get_or_404(post_id)
     return render_template('posts/post.html', post=post)
+
+
+@app.route('/post/<int:post_id>/update', methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+        post = Post.query.get_or_404(post_id)
+        if post.author != current_user:
+            abort(403)
+        form = PostForm()
+        if form.validate_on_submit():
+            post.title = form.title.data
+            post.content = form.content.data
+            db.session.commit()
+            flash('Your post has been updated!', 'success')
+            return redirect(url_for('post', post_id=post.id))
+        elif request.method == 'GET':
+            form.title.data = post.title
+            form.content.data = post.content
+        return render_template('posts/create_posts.html', form=form, legend='Update Post')
+
+
+@app.route('/post/<int:post_id>/delete', methods=['POST'])
+@login_required
+def delete_post(post_id):
+        post = Post.query.get_or_404(post_id)
+        if post.author != current_user:
+            abort(403)
+            db.session.delete(post)
+            db.session.commit()
+            flash('Your post has been deleted!', 'success')
+        return redirect(url_for('index'))
 
 
 @app.errorhandler(404)
