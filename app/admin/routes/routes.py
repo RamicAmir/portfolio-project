883fa28d5dc2d90b5cccd889eac5f48f2da03fa6
Amer Ahmed <amer.ahmed@studerande.plushogskolan.se>
2022-02-11 11:50:12 +0100

    Added comments for Admin-page

--- app/admin/routes/routes.py
+++ app/admin/routes/routes.py
@@ -15,6 +15,7 @@ admin = Blueprint('admin', __name__)
 
 @admin.route('/')
 def index():
+    # Home page
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.published.desc()).paginate(page=page, per_page=2)
     return render_template('index.html', posts=posts)
@@ -22,19 +23,23 @@ def index():
 
 @admin.route("/about")
 def about():
+    # About page
     return render_template('about.html')
 
 
 @admin.route('/contact')
 def contact():
+    # Contact page
     return render_template('contact.html')
 
 
 @admin.route('/projects')
 def projects():
+    # Projects for applications
     return render_template('profile/projects.html')
 
 
 @admin.route('/resume')
 def resume():
+    # User resume
     return render_template('profile/resume.html')
